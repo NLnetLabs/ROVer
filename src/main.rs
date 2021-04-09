9fbf23b8d5303813b487561876c26908ed0f444f
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2021-04-09 13:44:00 +0200

    Improved parsing test.

--- src/main.rs
+++ src/main.rs
@@ -168,8 +168,33 @@ mod tests {
 }
 "#;
 
-        let json: ValidityResponse = serde_json::from_str(data).unwrap();
+        let json: ValidityResponse = serde_json::from_str(routinator_validity_http_endpoint_output_sample).unwrap();
 
-        println!("{:?}", json);
+        assert_eq!(&json.validated_route.route.origin_asn, "AS43996");
+        assert_eq!(&json.validated_route.route.prefix, "5.57.16.0/24");
+
+        assert_eq!(&json.validated_route.validity.state, "valid");
+        assert_eq!(&json.validated_route.validity.description, "At least one VRP Matches the Route Prefix");
+
+        assert_eq!(json.validated_route.validity.vrps.matched.len(), 1);
+        assert_eq!(&json.validated_route.validity.vrps.matched[0].asn, "AS43996");
+        assert_eq!(&json.validated_route.validity.vrps.matched[0].prefix, "5.57.16.0/24");
+        assert_eq!(&json.validated_route.validity.vrps.matched[0].max_length, "24");
+
+        assert_eq!(json.validated_route.validity.vrps.unmatched_as.len(), 2);
+        assert_eq!(&json.validated_route.validity.vrps.unmatched_as[0].asn, "AS26415");
+        assert_eq!(&json.validated_route.validity.vrps.unmatched_as[0].prefix, "5.57.16.0/24");
+        assert_eq!(&json.validated_route.validity.vrps.unmatched_as[0].max_length, "24");
+        assert_eq!(&json.validated_route.validity.vrps.unmatched_as[1].asn, "AS19905");
+        assert_eq!(&json.validated_route.validity.vrps.unmatched_as[1].prefix, "5.57.16.0/24");
+        assert_eq!(&json.validated_route.validity.vrps.unmatched_as[1].max_length, "24");
+
+        assert_eq!(json.validated_route.validity.vrps.unmatched_length.len(), 2);
+        assert_eq!(&json.validated_route.validity.vrps.unmatched_length[0].asn, "AS43996");
+        assert_eq!(&json.validated_route.validity.vrps.unmatched_length[0].prefix, "5.57.16.0/21");
+        assert_eq!(&json.validated_route.validity.vrps.unmatched_length[0].max_length, "21");
+        assert_eq!(&json.validated_route.validity.vrps.unmatched_length[1].asn, "AS43996");
+        assert_eq!(&json.validated_route.validity.vrps.unmatched_length[1].prefix, "5.57.16.0/22");
+        assert_eq!(&json.validated_route.validity.vrps.unmatched_length[1].max_length, "22");
     }
 }
