d73cfc57dd24ae7eaad0c936da00f6e5294b34dd
Ximon Eighteen <3304436+ximon18@users.noreply.github.com>
2021-04-10 20:08:09 +0200

    Show the Routinator version in !about.

--- src/commands/about.rs
+++ src/commands/about.rs
@@ -5,6 +5,7 @@ use serenity::{
 };
 
 use crate::constants::APP_VERSION;
+use crate::types::StatusResponse;
 use crate::util::service_base_uri;
 
 #[command]
@@ -13,16 +14,34 @@ async fn about(ctx: &Context, msg: &Message) -> CommandResult {
         r#"
 ROVer - an open source bot by NLnet Labs for in-chat feedback about the state of the RPKI.
 
-Bot Version: {version}
-Service URI: {service_base_uri}
+ROVer Version  : {version}
+Service URI    : {service_base_uri}
+Service Version: {service_version}
 
 See https://github.com/NLnetLabs/ROVer for more information.
 "#,
         version = APP_VERSION,
-        service_base_uri = service_base_uri()
+        service_base_uri = service_base_uri(),
+        service_version = get_routinator_version().unwrap_or("Unavailable".to_string),
     );
 
     msg.reply(ctx, format!("```{}```", about)).await?;
 
     Ok(())
+}
+
+fn get_routinator_version() -> Result<String, String> {
+    let status_url = format!("{}/api/v1/status", service_base_uri());
+    match ureq::get(&status_url).call() {
+        Err(ureq::Error::Status(code, _)) => Err(format!("Version check failed: Status code {}", code)),
+        Err(_) => Err("Version check failed: Unable to contact the service".to_string()),
+        Ok(res) => {
+            let json_res: std::io::Result<StatusResponse> = res.into_json();
+
+            match json_res {
+                Err(err) => Err(format!("Version check failed: Bad response: {}", err)),
+                Ok(status_json) => Ok(status_json.version),
+            }
+        }
+    }
 }
